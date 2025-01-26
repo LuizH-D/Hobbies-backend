fec934231baae38da1ec1e7d5e32b163a965cd50
Luiz Henrique Dutra <luizh.dutra001@outlook.com>
2025-01-26 08:16:24 -0300

    Criação do projeto / Project creation

---
+++ src/main/java/com/LH_D/Hobbies/HobbiesApplication.java
@@ -0,0 +1,13 @@
+package com.LH_D.Hobbies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbiesApplication.class, args);
+	}
+
+}
